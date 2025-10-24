4944f9f6a1a29413f71d253f57e7484379807768
landanfp <159138257+landanfp@users.noreply.github.com>
2025-10-24 15:59:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '5355055672:AAHoidc0x6nM3g2JHmb7xhWKmwGJOoKFNXY'
+BOT_TOKEN = '8189638115:AAEYMDvummCXAPgdpavZbYHa3YuXpOzkRBY'
 
 app = Client("watermark-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
